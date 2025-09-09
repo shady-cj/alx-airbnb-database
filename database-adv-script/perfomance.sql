93c2bdde8293907cd905a93e392e8f5a622121f6
ERINFOLAMI PETER <66220414+shady-cj@users.noreply.github.com>
2025-09-09 14:56:15 +0100

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,22 +1,12 @@
 -- Write an initial query that retrieves all bookings along with the user details, property details, and payment details and save it on perfomance.sql
 
-SELECT * 
-FROM bookings
-JOIN users ON
-bookings.user_id = users.user_id
-JOIN properties ON
-properties.property_id = bookings.property_id
-JOIN payments ON
-payments.booking_id = bookings.booking_id;
+SELECT * FROM bookings, users, properties, payments WHERE
+bookings.user_id = users.user_id AND properties.property_id = bookings.property_id
+AND payments.booking_id = bookings.booking_id;
 
-EXPLAIN ANALYZE SELECT * 
-FROM bookings
-JOIN users ON
-bookings.user_id = users.user_id
-JOIN properties ON
-properties.property_id = bookings.property_id
-JOIN payments ON
-payments.booking_id = bookings.booking_id;
+EXPLAIN ANALYZE SELECT * FROM bookings, users, properties, payments WHERE
+bookings.user_id = users.user_id AND properties.property_id = bookings.property_id
+AND payments.booking_id = bookings.booking_id;
 
 CREATE INDEX bookings_user_idx ON bookings(user_id);
 CREATE INDEX bookings_property_idx ON bookings(property_id);
